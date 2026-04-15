53ca44dc2272843a7cd956289ddbec93f957e0db
odvalery <odingaval71@gmail.com>
2026-04-15 12:42:31 +0300

    feat(model): Added a Data Transfer Object struct to transfer the data in the format that is needed by the client side

--- internal/post/model.go
+++ internal/post/model.go
@@ -2,16 +2,25 @@ package post
 
 import "time"
 
+//How the data is store in the database
 type Post struct {
 	ID int
 	UserID int
-	Name string
 	Title string
 	Content string
-	categories []Category
-	likes, dislikes int
 	CreatedAt time.Time
 
+}
+
+//How the data will be presented to the user (DTO)
+type PostResponse struct {
+	ID int
+	Title string
+	Content string
+	Username string
+	Category []Category
+	Likes, Dislikes int
+	CreatedAt time.Time
 }
 
 type Category struct {
