66d4f40f3ba8a8226035e22017d9f43c26918e16
Ashomondi <adhiamboa745@gmail.com>
2026-04-16 19:47:14 +0300

    routes defines end point

--- internal/auth/routes.go
+++ internal/auth/routes.go
@@ -3,6 +3,21 @@ package auth
 import "net/http"
 
 func RegisterRoutes(handler *Handler) {
-	http.HandleFunc("/register", handler.Register)
-	http.HandleFunc("/login", handler.Login)
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			http.ServeFile(w, r, "web/templates/register.html")
+			return
+		}
+		handler.Register(w, r)
+	})
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			http.ServeFile(w, r, "web/templates/login.html")
+			return
+		}
+		handler.Login(w, r)
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "web/templates/home.html")
+	})
 }
