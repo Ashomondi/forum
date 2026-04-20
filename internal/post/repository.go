268fcee49ae2fd9d63a18885411d3e612d6ab5f2
odvalery <odingaval71@gmail.com>
2026-04-20 12:24:47 +0300

    feat(repo): Added GetPostByUser implementation in the repository layer

--- internal/post/repository.go
+++ internal/post/repository.go
@@ -64,8 +64,45 @@ func (r *PostRepository) GetPostByID(id int) (Post, error) {
 	return post, nil
 }
 
-func (r *PostRepository) GetPostByUser(user string) (Post, error) {
-	panic("Not implemented")
+func (r *PostRepository) GetPostByUser(userID int)([]Post, error) {
+	rows, err := r.db.Query(`
+	SELECT id, user_id, content, created_at
+	FROM posts
+	WHERE user_id = ? 
+	`,userID)
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var posts []Post
+
+	for rows.Next() {
+		var post Post
+
+		err := rows.Scan (
+			&post.ID,
+			&post.UserID,
+			&post.Title,
+			&post.Content,
+			&post.CreatedAt,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		posts = append(posts, post )
+	}
+
+	if err := rows.Err();err != nil {
+		return nil, err
+	}
+
+	return posts, nil
+
+
 }
 
 func (r *PostRepository) CreatePost(post Post) error {
