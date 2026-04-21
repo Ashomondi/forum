5a3f8fd2068ba27c84b3271e403d5d99d662402e
sjarso <stephenjacob815@gmail.com>
2026-04-21 08:12:32 +0300

    feat(session):validate a session

--- internal/session/service.go
+++ internal/session/service.go
@@ -11,12 +11,14 @@ import (
 type Service struct {
 	repo *Repository
 }
+
 // The Constructor: This connects the Brain to the Librarian
 func NewService(repo *Repository) *Service {
 	return &Service{
 		repo: repo,
 	}
 }
+
 // function creates a session when user logins or registers
 func (s *Service) StartSession(userid int) (string, error) {
 	// generate a unique ID
@@ -31,15 +33,16 @@ func (s *Service) StartSession(userid int) (string, error) {
 	}
 	return token, nil
 }
-func(s *Service) ValidateSession(token string)(int,error){
-userID,expiresAt,err:=s.repo.Get(token)
-if err !=nil{
-	return 0,err
-}
-//check if current time is equal to expiry time
-if time.Now()==expiresAt{
-	s.repo.Delete(token)
-	return 0,errors.New("this ticket is expired")
-}
-return userID, nil
+
+func (s *Service) ValidateSession(token string) (int, error) {
+	userID, expiresAt, err := s.repo.Get(token)
+	if err != nil {
+		return 0, err
+	}
+	// check if current time is equal to expiry time
+	if time.Now().After(expiresAt) {
+		s.repo.Delete(token)
+		return 0, errors.New("this ticket is expired")
+	}
+	return userID, nil
 }
