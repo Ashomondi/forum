52e0bbf24315b50db144fa3695bca4b7c26c0877
Ashomondi <adhiamboa745@gmail.com>
2026-04-22 11:29:38 +0300

    checking if there is an error in template file

--- internal/auth/routes.go
+++ internal/auth/routes.go
@@ -1,23 +1,38 @@
 package auth
 
-import "net/http"
+import (
+	// "fmt"
+	// "html/template"
+	"log"
+	"net/http"
+)
 
 func RegisterRoutes(handler *Handler) {
+	log.Println("Routes registered")
 	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
-			http.ServeFile(w, r, "web/templates/register.html")
+			//fmt.Println(r.Method)
+			// temp,err:=template.ParseFiles("./web/template/register.html")
+			// if err!=nil{
+			// 	fmt.Println("error:",err)
+			// 	return
+			// }
+			http.ServeFile(w, r, "web/template/register.html")
+			// temp.Execute(w,nil)
 			return
 		}
 		handler.Register(w, r)
+	
 	})
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
-			http.ServeFile(w, r, "web/templates/login.html")
+			http.ServeFile(w, r, "web/template/login.html")
 			return
 		}
 		handler.Login(w, r)
 	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "web/templates/home.html")
+		
+		http.ServeFile(w, r, "web/template/home.html")
 	})
 }
